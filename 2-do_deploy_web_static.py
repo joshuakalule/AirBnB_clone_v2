1956e5889eab14800a9250a004cc1b0a77f5cc29
PimehT <tareerepimeh@gmail.com>
2024-03-11 00:25:55 +0100

    Deploy archive

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """deploy archive created to web-01 and web-02 servers"""
 from fabric.api import env, local, put, run
-from fabric.operations import exists
 import os
 import sys
 
 env.hosts = ['100.26.213.55', '54.236.45.113']
+env.user = 'ubuntu'
+env.key_filename = os.path.expanduser('~/.ssh/school')
 
 
 def do_deploy(archive_path):
@@ -22,31 +23,22 @@ def do_deploy(archive_path):
     release_name = os.path.splitext(archive_name)[0]
     release_path = f"/data/web_static/releases/"
 
-    put(archive_path, '/tmp/')
+    try:
+        put(archive_path, '/tmp/')
 
-    run(f'mkdir -p {release_path}{release_name}')
-    run(f'tar -xzf /tmp/{archive_name} -C {release_path}{release_name}/')
+        run(f'sudo mkdir -p {release_path}{release_name}')
+        run(f'sudo tar -xzf /tmp/{archive_name} -C\
+        {release_path}{release_name}/')
 
-    run(f'rm /tmp/{archive_name}')
+        run(f'sudo rm /tmp/{archive_name}')
 
-    run('rm -rf /data/web_static/current')
+        run('sudo rm -rf /data/web_static/current')
 
-    run(f'ln -s {release_path}{release_name}/ /data/web_static/current')
+        run(f'sudo ln -s {release_path}{release_name}/\
+        /data/web_static/current')
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
 
     print("New version deployed!")
     return True
-
-
-if __name__ == "__main__":
-    for arg in sys.argv[1:]:
-        if arg.startswith('archive_path='):
-            archive_path = arg.split('=')[1]
-            break
-        else:
-            print("The archive_path argument was not provided.")
-            sys.exit(1)
-
-    if do_deploy(archive_path):
-        print("Deployment successful.")
-    else:
-        print("Deployment failed.")
