217f6a222d39442ae14e40f4acc7594deaec6d14
PimehT <tareerepimeh@gmail.com>
2024-03-31 12:45:38 +0100

    still trying

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,56 +1,48 @@
 #!/usr/bin/python3
-"""Compress web static package
-"""
-from fabric.api import *
-from datetime import datetime
-from os import path
-
+"""Deploy archive created to web-01 and web-02 servers"""
+from fabric.api import env, put, run
+import os
 
 env.hosts = ['100.26.213.55', '54.236.45.113']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/school'
+env.key_filename = os.path.expanduser('~/.ssh/school')
 
 
 def do_deploy(archive_path):
-    """Deploy web files to server
     """
-    try:
-        if not (path.exists(archive_path)):
-            return False
+    Function to deploy archive.
 
-        # upload archive
-        put(archive_path, '/tmp/')
-
-        # create target dir
-        timestamp = archive_path[-18:-4]
-        run('sudo mkdir -p /data/web_static/\
-releases/web_static_{}/'.format(timestamp))
-
-        # uncompress archive and delete .tgz
-        run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
-
-        # remove archive
-        run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
-
-        # move contents into host web_static
-        run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
-
-        # remove extraneous web_static dir
-        run('sudo rm -rf /data/web_static/releases/\
-web_static_{}/web_static'
-            .format(timestamp))
-
-        # delete pre-existing sym link
-        run('sudo rm -rf /data/web_static/current')
-
-        # re-establish symbolic link
-        run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
-    except:
+    Returns: False if archive_path does not exist.
+    """
+    if not os.path.exists(archive_path):
+        print(f"The file at the path {archive_path} does not exist.")
         return False
 
-    # return True on success
-    return True
+    archive_name = os.path.basename(archive_path)
+    release_name = os.path.splitext(archive_name)[0]
+    release_path = f"/data/web_static/releases/"
+
+    try:
+        put(archive_path, '/tmp/')
+
+        run(f'sudo mkdir -p {release_path}{release_name}/')
+        run(f'sudo tar -xzf /tmp/{archive_name} -C '
+            f'{release_path}{release_name}/')
+
+        run(f'sudo rm /tmp/{archive_name}')
+
+        run(f'sudo mv {release_path}{release_name}/web_static/* '
+            f'{release_path}{release_name}/')
+
+        run(f'sudo rm -rf {release_path}{release_name}/web_static')
+
+        run('sudo rm -rf /data/web_static/current')
+
+        run(f'sudo ln -s {release_path}{release_name} '
+            f'/data/web_static/current')
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print(f"An error occurred during deployment: {e}")
+        return False
