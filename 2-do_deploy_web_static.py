15c37c80499c580b0099adca134b12686da8ca3c
Kalule Joshua <kaljoshkalule@gmail.com>
2024-04-03 14:49:32 +0000

    resolve paths

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,43 +1,48 @@
 #!/usr/bin/python3
 """Distribute archive created to web-01 and web-02 servers"""
 from fabric.api import env, put, run
-from pathlib import Path
+import os
 
 env.hosts = ['52.206.61.240', '54.144.158.52']
 env.user = 'ubuntu'
-env.key_filename = Path.home() / '.ssh' / 'school'
+env.key_filename = os.path.expanduser('~/.ssh/server_rsa')
 
 
 def do_deploy(archive_path):
-    """Deploys archive."""
-    if not Path(archive_path).exists():
+    """
+    Function to deploy archive.
+
+    Returns: False if archive_path does not exist.
+    """
+    if not os.path.exists(archive_path):
+        print(f"The file at the path {archive_path} does not exist.")
         return False
 
-    release_name = Path(archive_path).stem
+    archive_name = os.path.basename(archive_path)
+    release_name = os.path.splitext(archive_name)[0]
     release_path = f"/data/web_static/releases/"
 
     try:
         put(archive_path, '/tmp/')
 
         run(f'sudo mkdir -p {release_path}{release_name}/')
-
-        tar_path = f'/tmp/{archive_name}'
-        dir_path = f'{release_path}{release_name}/'
-        run(f'sudo tar -xzf {tar_path} -C {dir_path}')
+        run(f'sudo tar -xzf /tmp/{archive_name} -C '
+            f'{release_path}{release_name}/')
 
         run(f'sudo rm /tmp/{archive_name}')
 
-        sources = f'{release_path}{release_name}/web_static/*'
-        dest_dir = f'{release_path}{release_name}/'
-        run(f'sudo mv {sources} {dest_dir}')
+        run(f'sudo mv {release_path}{release_name}/web_static/* '
+            f'{release_path}{release_name}/')
 
         run(f'sudo rm -rf {release_path}{release_name}/web_static')
 
         run('sudo rm -rf /data/web_static/current')
 
-        sym_link = f'{release_path}{release_name}'
-        run(f'sudo ln -s {sym_link} /data/web_static/current')
+        run(f'sudo ln -s {release_path}{release_name} '
+            f'/data/web_static/current')
+        print("New version deployed!")
         return True
 
     except Exception as e:
+        print(f"An error occurred during deployment: {e}")
         return False
