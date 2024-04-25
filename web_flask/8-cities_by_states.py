32d396e804e62b8cd7efc71049bf33003bcea314
Kalule Joshua <kaljoshkalule@gmail.com>
2024-04-25 23:20:23 +0000

    update task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-"""Starts a flask web application """
+""" starts a flask web application
+and shows cities by states"""
 from flask import Flask, render_template
+from models import *
 from models import storage
 from models.state import State
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(error):
-    """Removes the current SQLAlchemy Session."""
+def teardown_session(exception):
     storage.close()
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Fetch and render states."""
-    state_objs = list()
-    for k, obj in storage.all(State).items():
-        state_objs.append(obj)
-
-    return render_template('8-cities_by_states.html', states=state_objs)
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
