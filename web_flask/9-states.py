0405f2ecbd6d167b2f77aa4d556a27e0f7ea8e55
Kalule Joshua <kaljoshkalule@gmail.com>
2024-04-26 00:00:12 +0000

    update task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,10 +17,11 @@ def close(error):
 def states(id=None):
     """Fetch and render states."""
     state_objs = list()
-    found_state=None
+    found_state = None
     for k, obj in storage.all(State).items():
         state_objs.append(obj)
-        if id and k.endswith(id):
+        k_id = k.split('.')[1]
+        if str(id) == k_id:
             found_state = obj
 
     return render_template(
@@ -29,7 +30,6 @@ def states(id=None):
         id_arg=id,
         found_state=found_state
     )
-
 
 
 if __name__ == "__main__":
